0aba3e6fe0b8a2128bb6c59dd06b1a4761403ed4
Xuan <tanqazx@gmail.com>
2018-12-17 14:57:46 +0800

    Ported code for basic LSTM model.

--- datasets.py
+++ datasets.py
@@ -12,8 +12,8 @@ class MultiseqDataset(Dataset):
     """Multimodal dataset for time series and sequential data."""
     
     def __init__(self, modalities=None, dirs=None, regex=None,
-                 rates=None, base_rate=None, truncate=False,
-                 item_as_dict=False):
+                 rates=None, base_rate=None, preprocess=None,
+                 truncate=False, item_as_dict=False):
         """Loads valence ratings and features for each modality.
 
         modalities -- names of each input modality
@@ -21,60 +21,73 @@ class MultiseqDataset(Dataset):
         regex -- regex patterns for the filenames of each modality
         rates -- sampling rates of each modality
         base_rate -- base_rate to subsample to
+        preprocess -- data pre-processing functions for pandas dataframes
         truncate -- if true, truncate to modality with minimum length
         item_as_dict -- whether to return data as dictionary
         """
         # Store arguments
         self.modalities = modalities
-        if type(regex) is not list:
-            self.regex = [regex] * len(self.modalities)
-        else:
-            self.regex = regex
         if type(rates) is not list:
-            self.rates = [rates] * len(self.modalities)
+            self.rates = [rates] * len(modalities)
         else:
             self.rates = rates
         self.base_rate = base_rate if base_rate != None else min(self.rates)
         self.item_as_dict = item_as_dict
+
+        # Convert to modality-indexed dictionaries
+        dirs = {m: d for m, d in zip(modalities, dirs)}
+        if type(regex) is not list:
+            regex = [regex] * len(self.modalities)
+        regex = {m: r for m, r in zip(modalities, regex)}
+        if preprocess is None:
+            preprocess = lambda x : x
+        if type(preprocess) is not list:
+            preprocess = [preprocess] * len(self.modalities)
+        preprocess = {m: p for m, p in zip(modalities, preprocess)}
         
-        # Load filenames into lists
+        # Load filenames into lists and extract regex-captured sequence IDs
         paths = dict()
-        for i, m in enumerate(self.modalities):
+        seq_ids = dict()
+        for m in modalities:
             paths[m] = []
-            groups = []
-            for fn in os.listdir(dirs[i]):
-                match = re.match(self.regex[i], fn)
+            seq_ids[m] = []
+            for fn in os.listdir(dirs[m]):
+                match = re.match(regex[m], fn)
                 if not match:
                     continue
-                paths[m].append(os.path.join(dirs[i], fn))
-                groups.append(match.groups())
-            # Sort by values of captured groups
-            paths[m] = [p for _, p in sorted(zip(groups, paths[m]))]
-        self.indices = list(sorted(groups))
+                paths[m].append(os.path.join(dirs[m], fn))
+                seq_ids[m].append(match.groups())
+            # Sort by values of captured indices
+            paths[m] = [p for _, p in sorted(zip(seq_ids[m], paths[m]))]
+            seq_ids[m].sort()
 
-        # Check that number of files/sequences are equal
-        self.n_seq = len(paths[self.modalities[0]])
-        for m in self.modalities:
-            if len(paths[m]) != self.n_seq:
+        # Check that number and IDs of files/sequences are matched
+        self.seq_ids = seq_ids[modalities[0]]
+        for m in modalities:
+            if len(paths[m]) != len(self.seq_ids):
                 raise Exception("Number of files ({}) do not match.".\
                                 format(len(paths[m])))
+            if seq_ids[m] != self.seq_ids:
+                raise Exception("Sequence IDs do not match.")
 
         # Compute ratio to base rate
         self.ratios = {m: int(r/self.base_rate) for m, r in
                        zip(self.modalities, self.rates)}
             
         # Load data from files
-        self.data = {m: [] for m in self.modalities}
+        self.data = {m: [] for m in modalities}
         self.lengths = []
-        for i in range(self.n_seq):
+        for i in range(len(self.seq_ids)):
             seq_len = float('inf')
             # Load each input modality
             for m, data in self.data.iteritems():
                 fp = paths[m][i]
-                # Use pandas to read CSV files
                 if re.match("^.*\.(csv|txt)", fp):
-                    d = np.array(pd.read_csv(fp))
-                else: 
+                    # Use pandas to read and pre-process CSV files
+                    d = pd.read_csv(fp)
+                    d = np.array(preprocess[m](d))
+                else:
+                    # Otherwise load as numpy array
                     d = np.load(fp)
                 # Flatten inputs
                 if len(d.shape) > 2:
@@ -98,21 +111,16 @@ class MultiseqDataset(Dataset):
             self.lengths.append(seq_len)
             
     def __len__(self):
-        return self.n_seq
+        return len(self.seq_ids)
 
     def __getitem__(self, i):
         if self.item_as_dict:
             d = {m: self.data[m][i] for m in self.modalities}
             d['length'] = self.lengths[i]
+            return d
         else:
             return tuple(self.data[m][i] for m in self.modalities)
-    
-    def get_data(self, i, as_dict=False):
-        if as_dict:
-            return {m: self.data[m][i] for m in self.modalities}
-        else:
-            return self.__getitem__(i)
-    
+        
     def normalize_(self):
         """Rescale all inputs to [-1, 1] range (in-place)."""
         # Find max and min for each dimension of each modality
@@ -138,6 +146,8 @@ class MultiseqDataset(Dataset):
     def split_(self, n):
         """Splits each sequence into n chunks (in place)."""
         for m in self.modalities:
+            self.seq_ids = list(itertools.chain.from_iterable(
+                [[i] * n for i in self.seq_ids]))
             self.data[m] = list(itertools.chain.from_iterable(
                 [np.array_split(a, n, 0) for a in self.data[m]]))
 
@@ -155,7 +165,7 @@ class MultiseqDataset(Dataset):
         if (set1.base_rate != set2.base_rate):
             raise Exception("Base rates need to match.")
         merged = copy.deepcopy(set1)
-        merged.n_seq += set2.n_seq
+        merged.seq_ids += set2.seq_ids
         merged.rates = [merged.base_rate] * len(merged.modalities)
         for m in merged.modalities:
             merged.data[m] += set2.data[m]
@@ -177,6 +187,8 @@ def pad_and_merge(sequences, max_len=None):
     for i, seq in enumerate(sequences):
         end = lengths[i]
         padded_seqs[i, :end, :] = torch.from_numpy(seq[:end,:])
+    if len(sequences) == 1:
+        padded_seqs = padded_seqs.float()
     return padded_seqs
 
 def seq_collate(data):
@@ -198,7 +210,7 @@ def seq_collate(data):
 def seq_collate_dict(data):
     """Collate that accepts and returns dictionaries."""
     batch = {}
-    modalities = [k for k in data[0].keys() if k!='length']
+    modalities = [k for k in data[0].keys() if  k != 'length']
     data.sort(key=lambda d: d['length'], reverse=True)
     lengths = [d['length'] for d in data]
     for m in modalities:
@@ -218,17 +230,26 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     modalities = ['acoustic', 'emotient', 'ratings']
-    dirs = []
-    dirs.append(os.path.join(args.dir, 'features', args.subset, 'acoustic'))
-    dirs.append(os.path.join(args.dir, 'features', args.subset, 'emotient'))
-    dirs.append(os.path.join(args.dir, 'ratings', args.subset, 'target'))
-    regex = ["ID(\d+)_vid(\d+)_.*\.csv",
-             "ID(\d+)_vid(\d+)_.*\.txt",
-             "target_(\d+)_(\d+)_.*\.csv"]
+    dirs = [
+        os.path.join(args.dir, 'features', args.subset, 'acoustic'),
+        os.path.join(args.dir, 'features', args.subset, 'emotient'),
+        os.path.join(args.dir, 'ratings', args.subset, 'target')
+    ]
+    regex = [
+        "ID(\d+)_vid(\d+)_.*\.csv",
+        "ID(\d+)_vid(\d+)_.*\.txt",
+        "target_(\d+)_(\d+)_.*\.csv"
+    ]
     rates = [2, 30, 2]
+    preprocess = [
+        lambda df : df.drop(columns=['frameIndex', ' frameTime']),
+        lambda df : df.drop(columns=['Frametime']),
+        lambda df : df.drop(columns=['time'])
+    ]
     
     print("Loading data...")
-    dataset = MultiseqDataset(modalities, dirs, regex, rates)
+    dataset = MultiseqDataset(modalities, dirs, regex, rates,
+                              preprocess=preprocess)
     print("Testing batch collation...")
     data = seq_collate([dataset[i] for i in range(min(10, len(dataset)))])
     print("Batch shapes:")
@@ -237,7 +258,7 @@ if __name__ == "__main__":
     print("Sequence lengths: ", data[-1])
     print("Checking through data for mismatched sequence lengths...")
     for i, data in enumerate(dataset):
-        print("Subject, Video: ", dataset.indices[i])
+        print("Subject, Video: ", dataset.seq_ids[i])
         acoustic, emotient, ratings = data
         print(acoustic.shape, emotient.shape, ratings.shape)
         if not (len(acoustic) == len(emotient) and
---
+++ lstm.py
@@ -0,0 +1,150 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+class MultiLSTM(nn.Module):
+    """Multimodal LSTM model with feature level fusion.
+
+    modalities -- list of names of each input modality
+    dims -- list of dimensions for input modalities
+    embed_dim -- dimensions of embedding for feature-level fusion
+    hidden_dim -- dimensions of LSTM hidden state
+    n_layers -- number of LSTM layers
+    attn_len -- length of local attention window
+    """
+    
+    def __init__(self, modalities, dims, embed_dim=128, hidden_dim=512,
+                 n_layers=1, attn_len=3, device=torch.device('cuda:0')):
+        super(MultiLSTM, self).__init__()
+        self.modalities = modalities
+        self.n_mods = len(modalities)
+        self.dims = dict(zip(modalities, dims))
+        self.embed_dim = embed_dim
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+        self.attn_len = attn_len
+        
+        # Create raw-to-embed FC+Dropout layer for each modality
+        self.embed = dict()
+        for m in self.modalities:
+            self.embed[m] = nn.Sequential(nn.Linear(self.dims[m], embed_dim),
+                                          nn.ReLU())
+            self.add_module('embed_{}'.format(m), self.embed[m])
+        # Layer that computes attention from embeddings
+        self.attn = nn.Sequential(nn.Linear(self.n_mods*embed_dim, embed_dim),
+                                  nn.ReLU(),
+                                  nn.Linear(embed_dim, attn_len),
+                                  nn.Softmax(dim=1))
+        # LSTM computes hidden states from embeddings for each modality
+        self.lstm = nn.LSTM(self.n_mods*embed_dim, hidden_dim,
+                            n_layers, batch_first=True)
+        # Regression network from LSTM hidden states to predicted valence
+        self.dec_target = nn.Sequential(nn.Linear(hidden_dim, embed_dim),
+                                        nn.ReLU(),
+                                        nn.Linear(embed_dim, 1))
+        # Store module in specified device (CUDA/CPU)
+        self.device = (device if torch.cuda.is_available() else
+                       torch.device('cpu'))
+        self.to(self.device)
+
+    def forward(self, inputs, mask, lengths, output_features=False):
+        # Get batch dim
+        batch_size, seq_len = len(lengths), max(lengths)
+        # Convert raw features into equal-dimensional embeddings
+        embed = torch.cat([self.embed[m](inputs[m].view(-1, self.dims[m]))
+                           for m in self.modalities], 1)
+        # Compute attention weights
+        attn = self.attn(embed)
+        # Unflatten temporal dimension
+        embed = embed.reshape(batch_size, seq_len, self.n_mods*self.embed_dim)
+        attn = attn.reshape(batch_size, seq_len, self.attn_len)
+        # Pack the input to mask padded entries
+        embed = pack_padded_sequence(embed, lengths, batch_first=True)
+        # Set initial hidden and cell states
+        h0 = torch.zeros(self.n_layers, batch_size,
+                         self.hidden_dim).to(self.device)
+        c0 = torch.zeros(self.n_layers, batch_size,
+                         self.hidden_dim).to(self.device)
+        # Forward propagate LSTM
+        h, _ = self.lstm(embed, (h0, c0))
+        # Undo the packing
+        h, _ = pad_packed_sequence(h, batch_first=True)
+        # Convolve output with attention weights
+        # i.e. out[t] = a[t,0]*in[t] + ... + a[t,win_len-1]*in[t-(win_len-1)]
+        stacked = torch.stack([self.pad_shift(h, i) for
+                               i in range(self.attn_len)], dim=-1)
+        context = torch.sum(attn.unsqueeze(2) * stacked, dim=-1)
+        # Flatten temporal dimension
+        context = context.reshape(-1, self.hidden_dim)
+        # Return features before final FC layer if flag is set
+        if output_features:
+            features = self.dec_target[0](context)
+            features = features.view(batch_size, seq_len, -1) * mask.float()
+            return features
+        # Decode the context for each time step
+        target = self.dec_target(context).view(batch_size, seq_len, 1)
+        # Mask target entries that exceed sequence lengths
+        target = target * mask.float()
+        return target
+
+    def pad_shift(self, x, shift):
+        """Shift 3D tensor forwards in time with zero padding."""
+        if shift > 0:
+            padding = torch.zeros(x.size(0), shift, x.size(2)).to(self.device)
+            return torch.cat((padding, x[:, :-shift, :]), dim=1)
+        elif shift < 0:
+            padding = torch.zeros(x.size(0), -shift, x.size(2)).to(self.device)
+            pad = torch.cat((x[:, -shift:, :], padding), dim=1)
+            return pad
+        else:
+            return x
+    
+if __name__ == "__main__":
+    # Test code by loading dataset and running through model
+    import os, argparse
+    from datasets import MultiseqDataset, seq_collate_dict
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', type=str, default="./data",
+                        help='data directory')
+    parser.add_argument('--subset', type=str, default="Train",
+                        help='whether to load Train/Valid/Test data')
+    args = parser.parse_args()
+
+    modalities = ['acoustic', 'emotient', 'ratings']
+    dirs = [
+        os.path.join(args.dir, 'features', args.subset, 'acoustic'),
+        os.path.join(args.dir, 'features', args.subset, 'emotient'),
+        os.path.join(args.dir, 'ratings', args.subset, 'target')
+    ]
+    regex = [
+        "ID(\d+)_vid(\d+)_.*\.csv",
+        "ID(\d+)_vid(\d+)_.*\.txt",
+        "target_(\d+)_(\d+)_.*\.csv"
+    ]
+    rates = [2, 30, 2]
+    preprocess = [
+        lambda df : df.drop(columns=['frameIndex', ' frameTime']),
+        lambda df : df.drop(columns=['Frametime']),
+        lambda df : df.drop(columns=['time'])
+    ]
+    
+    print("Loading data...")
+    dataset = MultiseqDataset(modalities, dirs, regex, rates,
+                              preprocess=preprocess, truncate=True,
+                              item_as_dict=True)
+    print("Building model...")
+    model = MultiLSTM(['acoustic', 'emotient'], [988, 31],
+                      device=torch.device('cpu'))
+    model.eval()
+    print("Passing a sample through the model...")
+    data, mask, lengths = seq_collate_dict([dataset[0]])
+    del data['ratings']
+    out = model(data, mask, lengths).view(-1)
+    print("Predicted valences:")
+    for o in out:
+        print("{:+0.3f}".format(o.item()))
